cd9627d5ae50544ef9bfea50b7f74ad536727307
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2022-11-15 23:38:49 +0100

    initialized spring boot kafka producer

---
+++ src/test/java/miaplatform/core/pollinator/PollinatorApplicationTests.java
@@ -0,0 +1,13 @@
+	package miaplatform.core.pollinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
